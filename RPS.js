2b994292c351a5e82106d9ce0270c8963f28dc1d
John Spruijt <99058314@mydavinci.nl>
2019-10-21 22:46:23 +0200

    visual side is done

--- RPS.js
+++ RPS.js
@@ -1,7 +1,26 @@
 function hidestuff() {
     var hidegoback = document.getElementById('goBack')
     var hideoptions = document.getElementById('firstpage')
-    if (hideoptions.style.display == "none") {
+    var prockwin = document.getElementById('playerrockwin') //variables to show and hide results
+    var procktie = document.getElementById('playerrocktie')
+    var procklose = document.getElementById('playerrocklose')
+    var ppaperwin = document.getElementById('playerpaperwin')
+    var ppapertie = document.getElementById('playerpapertie')
+    var ppaperlose = document.getElementById('playerpaperlose')
+    var pscissorswin = document.getElementById('playerscissorswin')
+    var pscissorstie = document.getElementById('playerscissorstie')
+    var pscissorslose = document.getElementById('playerscissorslose')
+
+    prockwin.style.display = "none" //hides results
+    procktie.style.display = "none"
+    procklose.style.display = "none"
+    ppaperwin.style.display = "none"
+    ppapertie.style.display = "none"
+    ppaperlose.style.display = "none"
+    pscissorswin.style.display = "none"
+    pscissorstie.style.display = "none"
+    pscissorslose.style.display = "none"
+    if (hideoptions.style.display == "none") { //resets page
         hidegoback.style.display = "none"
         hideoptions.style.display = "block"
     }
@@ -12,42 +31,51 @@ function hidestuff() {
 }
 
 function rps() {
-    var playerOutput = document.querySelector('input[name="playerInput"]:checked').value;
-    var computerInput = Math.floor(Math.random() * (4 - 1) + 1)
+    var playerOutput = document.querySelector('input[name="playerInput"]:checked').value; //requests the answer the player inserts
+    var computerInput = Math.floor(Math.random() * (4 - 1) + 1) //random number generator
+    var prockwin = document.getElementById('playerrockwin') //variables to show and hide results
+    var procktie = document.getElementById('playerrocktie')
+    var procklose = document.getElementById('playerrocklose')
+    var ppaperwin = document.getElementById('playerpaperwin')
+    var ppapertie = document.getElementById('playerpapertie')
+    var ppaperlose = document.getElementById('playerpaperlose')
+    var pscissorswin = document.getElementById('playerscissorswin')
+    var pscissorstie = document.getElementById('playerscissorstie')
+    var pscissorslose = document.getElementById('playerscissorslose')
     if (playerOutput == "rock") {
         hidestuff()
         if (computerInput == 1) {
-            alert("You won!")
+            prockwin.style.display = "block"
         }
         else if (computerInput == 2) {
-            alert("You tied!")
+            procktie.style.display = "block"
         }
         else if (computerInput == 3) {
-            alert("You lost!")
+            procklose.style.display = "block"
         }
     }
     else if (playerOutput == "paper") {
         hidestuff()
         if (computerInput == 1) {
-            alert("You won!")
+            ppaperwin.style.display = "block"
         }
         else if (computerInput == 2) {
-            alert("You tied!")
+            ppapertie.style.display = "block"
         }
         else if (computerInput == 3) {
-            alert("You lost!")
+            ppaperlose.style.display = "block"
         }
     }
     else if (playerOutput == "scissors") {
         hidestuff()
         if (computerInput == 1) {
-            alert("You won!")
+            pscissorswin.style.display = "block"
         }
         else if (computerInput == 2) {
-            alert("You tied!")
+            pscissorstie.style.display = "block"
         }
         else if (computerInput == 3) {
-            alert("You lost!")
+            pscissorslose.style.display = "block"
         }
     }
 }
